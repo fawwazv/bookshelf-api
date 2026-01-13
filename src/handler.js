02aec3c0ffb7fa79b1cd7c717fb9b460dffa6870
Fawwazul Haq <mhmdfwwz7@gmail.com>
2026-01-13 23:23:08 +0700

    perubahan hampir akhir

--- src/handler.js
+++ src/handler.js
@@ -159,7 +159,23 @@ const editBookByIdHandler = (request, h) => {
 const deleteBookByIdHandler = (request, h) => {
     const { id } = request.params;
 
-    
+    const index = books.findIndex((book) => book.id === id);
+
+    if(index !== -1) {
+        books.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus',
+        });
+        response.code(200);
+        return response;
+    }
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
 }
 
 module.exports = {
@@ -167,4 +183,5 @@ module.exports = {
     getAllBooksHandler,
     getBookByIdHandler,
     editBookByIdHandler,
+    deleteBookByIdHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
-const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler } = require('./handler')
+const { addBookHandler, 
+    getAllBooksHandler, 
+    getBookByIdHandler, 
+    editBookByIdHandler, 
+    deleteBookByIdHandler} = require('./handler')
 
 const routes = [
     {
@@ -20,6 +24,11 @@ const routes = [
         method: 'PUT',
         path: '/books/{id}',
         handler: editBookByIdHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler
     }
 ]
 
